4ac13725084f51d9a051bfb00a7766c2df997da3
Armando Serna Gutiérrez <armandoserna97@gmail.com>
2018-03-08 14:39:28 -0600

    Eliminar el metodo aumentar y pasarlo a main
    Se agregaron comentarios con las observaciones de distintas ejecuciones del programa.
    Se agrego un letrero que ayuda a contar los decimales.

--- taylor/Taylor.java
+++ taylor/Taylor.java
@@ -45,23 +45,6 @@ public class Taylor {
     return suma;
   }
 
-
-  public static double aumentar (int numCoef, double c, double tolerancia) {
-    double error, piaprox;
-    double[] polinomio;
-    do {
-      polinomio = coefSen(numCoef, c);
-      piaprox = falsaPosicion(polinomio, c, tolerancia);
-      error = Math.abs(((Math.PI - piaprox) / Math.PI) * 100);
-      System.out.println("Sumandos");
-      for (int i = 0; i < polinomio.length; i ++)
-        System.out.println("\t" + polinomio[i] + "\tx^" + i);
-      System.out.println("Error para " + polinomio.length + " sumandos: " + error + "\n");
-      numCoef ++;
-    } while (error >= tolerancia);
-    return piaprox;
-  }
-
   public static double falsaPosicion (double polinomio[], double c, double tolerancia) {
     double f0, ff, fr, error;
     double x0 = 3, xf = 3.5, xr = 0, xra;
@@ -82,6 +65,7 @@ public class Taylor {
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    System.out.println("Calcula hasta 16 decimales de PI");
     System.out.print("Centrar la serie en: ");
     double c = scan.nextDouble();
 
@@ -92,8 +76,46 @@ public class Taylor {
     int cifras = scan.nextInt();
     double tolerancia = 0.5 * Math.pow(10, (2-cifras));
 
-    double resultado = aumentar(sumandos, c, tolerancia);
-    System.out.println("\nValor aprox. de PI: " + resultado);
-    System.out.println("Valor real de PI: " + Math.PI);
+    double error, piaprox;
+    double[] polinomio;
+    do {
+      polinomio = coefSen(sumandos, c);
+      piaprox = falsaPosicion(polinomio, c, tolerancia);
+      error = Math.abs(((Math.PI - piaprox) / Math.PI) * 100);
+      System.out.println("Sumandos");
+      for (int i = 0; i < polinomio.length; i ++)
+        System.out.println("\t" + polinomio[i] + "\tx^" + i);
+      System.out.println("Error para " + polinomio.length + " sumandos: " + error + "\n");
+      sumandos ++;
+    } while (error >= tolerancia);
+    System.out.print("\t\t\t1 ");
+    for (int i = 2; i <= 9; i ++) System.out.print(i);
+    for (int i = 0; i <= 8; i ++) System.out.print(i);
+    System.out.println("\nValor aprox. de PI:\t" + String.format("%.17f",piaprox));
+    System.out.println("Valor real de PI:\t" + String.format("%.17f",Math.PI));
   }
 }
+
+
+//Centrada en 14, necesita al menos 28 sumandos para que arroje un resultado
+//Centrada en 12, necesita al menos 23 sumandos para que arroje un resultado
+//Centrada en 10, necesita al menos 18 sumandos para que arroje un resultado
+//Centrada en 8, necesita al menos 13 sumandos para que arroje un resultado
+//Centrada en 6, necesita al menos 7 sumandos para que arroje un resultado
+
+//Centrada en 15 necesita 51 sumandos para 12 decimales
+//Centrada en 14 necesita 49 sumandos para 13 decimales
+//Centrada en 13 necesita 48 sumandos para 14 decimales
+//Centrada en 12 necesita 45 sumandos para 15 decimales
+//Centrada en 11 necesita 41 sumandos para 13 decimales
+//Centrada en 10 necesita 40 sumandos para 14 decimales
+//Centrada en 9 necesita 36 sumandos para 15 decimales
+//Centrada en 8 necesita 33 sumandos para 15 decimales
+//Centrada en 7 necesita 31 sumandos para 16 decimales
+//Centrada en 6 necesita 27 sumandos para 16 decimales
+//Centrada en 5 necesita 23 sumandos para 16 decimales
+//Centrada en 4 necesita 17 sumandos para 16 decimales
+//Centrada en 3 necesita 11 sumandos para 16 decimales
+//Centrada en 2 necesita 19 sumandos para 16 decimales
+//Centrada en 1 necesita 24 sumandos para 16 decimales
+//Centrada en 0 necesita 28 sumandos para 16 decimales
